7e19f9c411a32bd6e300a2ed704e0ca40191c8a9
chencl1986 <chencl1986@qq.com>
2019-01-15 23:32:30 +0800

    介绍RN样式

---
+++ lesson01/App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { calc } from './utils/common'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: 'column', height: '100%' }}>
+          <Text style={{ height: calc(80), backgroundColor: 'green', textAlign: 'center', lineHeight: calc(80) }}>Header</Text>
+          <Text style={{ flex: 1, backgroundColor: 'yellow' }}>aaa</Text>
+          <Text style={{ flex: 1, backgroundColor: 'red' }}>bbb</Text>
+        </View>
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text> */}
+        {/* <Text style={styles.instructions}>To get started, edit App.js</Text> */}
+        {/* <Text style={styles.instructions}>{instructions}</Text> */}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
