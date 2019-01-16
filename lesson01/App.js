f17e1003cabef9df5f9fc1e68ce73944561e9403
chencl1986 <chencl1986@qq.com>
2019-01-16 22:53:13 +0800

    React Native教程02：基础组件和样式介绍（完结）

--- lesson01/App.js
+++ lesson01/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { calc } from './utils/common'
 
 const instructions = Platform.select({
@@ -22,14 +22,32 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flexDirection: 'column', height: '100%' }}>
-          <Text style={{ height: calc(80), backgroundColor: 'green', textAlign: 'center', lineHeight: calc(80) }}>Header</Text>
-          <Text style={{ flex: 1, backgroundColor: 'yellow' }}>aaa</Text>
-          <Text style={{ flex: 1, backgroundColor: 'red' }}>bbb</Text>
-        </View>
-        {/* <Text style={styles.welcome}>Welcome to React Native!</Text> */}
-        {/* <Text style={styles.instructions}>To get started, edit App.js</Text> */}
-        {/* <Text style={styles.instructions}>{instructions}</Text> */}
+        <ScrollView
+          style={{ height: '100%' }}
+        >
+          <View>
+            <Image
+              style={{ width: 50, height: 50 }}
+              source={require('./unnamed.jpg')}
+            />
+            <Image
+              style={{ width: 50, height: 50 }}
+              source={{ uri: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2205261245,3424157468&fm=26&gp=0.jpg' }}
+            />
+            <Image
+              style={{ width: 66, height: 58 }}
+              source={{ uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==' }}
+            />
+          </View>
+          <View style={{ flexDirection: 'column', height: 800 }}>
+            <Text style={{ height: calc(80), backgroundColor: 'green', textAlign: 'center', lineHeight: calc(80) }}>Header</Text>
+            <Text style={{ flex: 1, backgroundColor: 'yellow' }}>aaa</Text>
+            <Text style={{ flex: 1, backgroundColor: 'red' }}>bbb</Text>
+          </View>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </ScrollView>
       </View>
     );
   }
